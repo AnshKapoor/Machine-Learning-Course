0c8086b07fa23cc471b515a1241a15a79d05bcb9
AnshKapoor <ansh605@gmail.com>
2018-11-07 00:11:40 +0530

    Made training and test set

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/temp.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/temp.py
@@ -17,5 +17,8 @@ onehotencoder = OneHotEncoder(categorical_features=[0])
 X = onehotencoder.fit_transform(X).toarray()
 labelencoder_Y = LabelEncoder()
 Y = labelencoder_Y.fit_transform(Y)
+# 
+from sklearn.cross_validation import train_test_split
+X_train,Y_train,X_test,Y_test = train_test_split(X,Y,test_size=0.20,random_state=0)
 
 
