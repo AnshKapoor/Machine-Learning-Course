eadb71077aa4bc6d734c2852c94676d49a52fc27
AnshKapoor <ansh605@gmail.com>
2018-11-07 00:51:03 +0530

    Performed feature scaling on the data

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/temp.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/temp.py
@@ -19,6 +19,11 @@ labelencoder_Y = LabelEncoder()
 Y = labelencoder_Y.fit_transform(Y)
 # 
 from sklearn.cross_validation import train_test_split
-X_train,Y_train,X_test,Y_test = train_test_split(X,Y,test_size=0.20,random_state=0)
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.20,random_state=0)
 
 
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
