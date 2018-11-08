cb6b0c19bd22a879ca032a70ef283de1fd68f0bf
AnshKapoor <ansh605@gmail.com>
2018-11-08 15:55:54 +0530

    Fitted dataset into a linear regression model

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/Simple_Linear_Regression/Simple_Linear_Regression/simple_lr.py
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/Simple_Linear_Regression/Simple_Linear_Regression/simple_lr.py
@@ -16,7 +16,7 @@ Y = dataset.iloc[:,1].values
 
 
 from sklearn.cross_validation import train_test_split
-X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.20,random_state=0)
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=1/3,random_state=0)
 
 
 # Feature Scaling
@@ -24,3 +24,6 @@ X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.20,random_state
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)"""
+from sklearn.linear_model import LinearRegression
+regressor =LinearRegression()
+regressor.fit(X_train,Y_train)
